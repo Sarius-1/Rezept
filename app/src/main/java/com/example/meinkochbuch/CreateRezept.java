39646990e0f6aa14a8e194a618a52b20a7aa8497
LeonRackisch <leon-joel.rackisch@hsw-stud.de>
2025-05-20 21:46:31 +0200

    Zutaten dynamische Liste -> Zutaten können auch wieder entfernt werden

--- app/src/main/java/com/example/meinkochbuch/CreateRezept.java
+++ app/src/main/java/com/example/meinkochbuch/CreateRezept.java
@@ -5,12 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.meinkochbuch.R;
-import com.google.android.material.textfield.TextInputEditText;
 
 public class CreateRezept extends Fragment {
 
@@ -20,16 +20,25 @@ public class CreateRezept extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_create_rezept, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_create_rezept, container, false);
 
-        zutatenContainer = view.findViewById(R.id.container_zutaten);
-        btnZutatHinzufuegen = view.findViewById(R.id.btn_zutat_hinzufuegen);
+        zutatenContainer = rootView.findViewById(R.id.container_zutaten);
+        btnZutatHinzufuegen = rootView.findViewById(R.id.btn_zutat_hinzufuegen);
 
-        btnZutatHinzufuegen.setOnClickListener(v -> {
-            View zutatView = inflater.inflate(R.layout.item_zutat, null);
+        // Funktion zum Hinzufügen eines neuen Zutaten-Views
+        View.OnClickListener addZutatView = v -> {
+            View zutatView = inflater.inflate(R.layout.item_zutat, zutatenContainer, false);
+            ImageButton btnEntfernen = zutatView.findViewById(R.id.btn_entfernen);
+            btnEntfernen.setOnClickListener(v1 -> zutatenContainer.removeView(zutatView));
             zutatenContainer.addView(zutatView);
-        });
+        };
 
-        return view;
+        // Button klick -> neues Feld hinzufügen
+        btnZutatHinzufuegen.setOnClickListener(addZutatView);
+
+        // Direkt ein erstes Feld anzeigen
+        addZutatView.onClick(null);
+
+        return rootView;
     }
 }
